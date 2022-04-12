14d49197b8ad2ba2b221ce03b2fbfabfd2527ad3
jswong <jswong@dynafront.com>
2022-04-12 10:38:33 +0800

    update config settings

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
-// module.exports = {
-// 	publicPath: process.env.NODE_ENV === 'production' ? '/sakai-vue' : '/'
-// }
+//module.exports = {
+//  publicPath: process.env.NODE_ENV === 'production' ? '/sakai-vue' : '/'
+//};
 
 module.exports = {
-  publicPath: "/my-vue-project/",
+  publicPath: "",
 };
